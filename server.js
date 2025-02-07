2e37b8eb53116fef15d63e7d31f62d23f4455e78
Bryan Villanueva <vbryan0803@gmail.com>
2025-02-08 10:00:24 +1100

    cambio de puerto

--- server.js
+++ server.js
@@ -118,4 +118,4 @@ process.on("SIGTERM", () => {
 
 // Inicia el servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
